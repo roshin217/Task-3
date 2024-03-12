4635c75eb8c6baf8d3cd6844fbf7b4a083df3e66
roshin217 <fry0402@gmail.com>
2024-03-12 12:01:08 +0500

    add Dog

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -3,9 +3,9 @@ package app.model;
 import org.springframework.stereotype.Component;
 
 @Component()
-public class Cat extends Animal {
+public class Dog extends Animal{
     @Override
     public String toString() {
-        return "Im a Cat";
+        return "Im a Dog";
     }
 }
